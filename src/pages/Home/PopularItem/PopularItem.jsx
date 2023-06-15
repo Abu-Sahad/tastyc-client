90ed2dcfbfd8607dfcd913b8a8943ffdb5483e26
Abu-Sahad <abusahad603@gmail.com>
2023-06-15 09:45:51 +0600

    added offer salad pizza soup item

--- src/pages/Home/PopularItem/PopularItem.jsx
+++ src/pages/Home/PopularItem/PopularItem.jsx
@@ -1,17 +1,21 @@
 import { useEffect, useState } from "react";
 import SectionTittle from "../../../components/SectionTittle/SectionTittle";
 import MenuItem from "../../../Shared/MenuItem/MenuItem";
+import useMenu from "../../../hooks/useMenu";
 
 const PopularItem = () => {
-    const [menu, setMenu] = useState([])
-    useEffect(() => {
-        fetch('menu.json')
-            .then(res => res.json())
-            .then(data => {
-                const popularItem = data.filter(item => item.category === 'popular')
-                setMenu(popularItem)
-            })
-    }, [])
+    // const [menu, setMenu] = useState([])
+    // useEffect(() => {
+    //     fetch('menu.json')
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             const popularItem = data.filter(item => item.category === 'popular')
+    //             setMenu(popularItem)
+    //         })
+    // }, [])
+
+    const [menu]=useMenu()
+
     return (
         <section className="mb-12">
             <SectionTittle
@@ -20,7 +24,7 @@ const PopularItem = () => {
             ></SectionTittle>
             <div className="grid md:grid-cols-2 gap-14">
                 {
-                    menu.map(item => <MenuItem
+                    menu.filter(item => item.category === 'popular').map(item => <MenuItem
                         key={item._id}
                         item={item}
                     ></MenuItem>)
--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -6,8 +6,11 @@ import Desert from "../Desert/Desert";
 import Pizza from "../Pizza/Pizza";
 import Salad from "../Salad/Salad";
 import Sopu from "../Soup/Sopu";
+import MenuItem from "../../../Shared/MenuItem/MenuItem";
+import useMenu from "../../../hooks/useMenu";
+import SectionTittle from "../../../components/SectionTittle/SectionTittle";
 const Menu = () => {
-    console.log(menuImg)
+    const [menu] = useMenu()
     return (
         <div>
             <Helmet>
@@ -18,15 +21,53 @@ const Menu = () => {
                 tittle='OUR MENU'
                 text='Would You Like To Try a Disk'
             ></Cover>
-            <PopularItem></PopularItem>
+
+            <SectionTittle heading='todays offer' subHeading='Do Not miss'></SectionTittle>
+
+            <div className="grid md:grid-cols-2 gap-14 mb-10 mt-10">
+                {
+                    menu.filter(item => item.category === 'offered').map(item => <MenuItem
+                        key={item._id}
+                        item={item}
+                    ></MenuItem>)
+                }
+            </div>
             <Desert></Desert>
-            <PopularItem></PopularItem>
+            <div className="grid md:grid-cols-2 gap-14 mb-10 mt-10">
+                {
+                    menu.filter(item => item.category === 'dessert').map(item => <MenuItem
+                        key={item._id}
+                        item={item}
+                    ></MenuItem>)
+                }
+            </div>
             <Pizza></Pizza>
-            <PopularItem></PopularItem>
+            <div className="grid md:grid-cols-2 gap-14 mb-10 mt-10">
+                {
+                    menu.filter(item => item.category === 'pizza').map(item => <MenuItem
+                        key={item._id}
+                        item={item}
+                    ></MenuItem>)
+                }
+            </div>
             <Salad></Salad>
-            <PopularItem></PopularItem>
+            <div className="grid md:grid-cols-2 gap-14 mb-10 mt-10">
+                {
+                    menu.filter(item => item.category === 'salad').map(item => <MenuItem
+                        key={item._id}
+                        item={item}
+                    ></MenuItem>)
+                }
+            </div>
             <Sopu></Sopu>
-            <PopularItem></PopularItem>
+            <div className="grid md:grid-cols-2 gap-14 mb-10 mt-10">
+                {
+                    menu.filter(item => item.category === 'soup').map(item => <MenuItem
+                        key={item._id}
+                        item={item}
+                    ></MenuItem>)
+                }
+            </div>
         </div>
     );
 };
